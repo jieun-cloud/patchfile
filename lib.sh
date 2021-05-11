19449209784af63af27a7a6a2f95a19f19902fff
Jieun <pipiato@kaist.ac.kr>
2021-05-11 10:46:45 +0900

    script file modified: git clone code added

--- lib.sh
+++ lib.sh
@@ -1,1 +1,3 @@
 sudo apt-get install vim git ctags cscope libaio-dev libncurses5-dev libncursesw5-dev
+git clone https://github.com/ESOS-Lab/barrieriostack.git
+git clone https://github.com/ESOS-Lab/Mobibench.git
