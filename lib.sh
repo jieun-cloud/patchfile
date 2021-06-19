5bc9f6d242582d4465579446506bd1692e826339
Jieun <pipiato@kaist.ac.kr>
2021-06-19 16:19:39 +0900

    library script is modified

--- lib.sh
+++ lib.sh
@@ -1,4 +1,4 @@
-sudo apt-get install vim git ctags cscope libaio-dev libncurses5-dev libncursesw5-dev
+sudo apt-get install -y vim git ctags cscope libaio-dev libncurses5-dev libncursesw5-dev
 git clone https://github.com/ESOS-Lab/barrieriostack.git
 git clone https://github.com/ESOS-Lab/Mobibench.git
 mv ./barrieriostack /home/jieun
