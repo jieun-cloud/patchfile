05841af6720985ab39da610d1563ff1d1e43d536
Jieun <pipiato@kaist.ac.kr>
2021-06-19 15:16:13 +0900

    filebench script is modified

--- filebench_lib_script.sh
+++ filebench_lib_script.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 git clone https://github.com/filebench/filebench.git
-sudo apt-get install automake
-sudo apt-get install libtool
-sudo apt-get install bison flex
-sudo apt-get install make
+sudo apt-get install -y automake
+sudo apt-get install -y libtool
+sudo apt-get install -y bison flex
+sudo apt-get install -y make
