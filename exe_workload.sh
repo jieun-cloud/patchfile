13d85182dac6ebe8d95b32570804377e3731e5a1
Jieun <pipiato@kaist.ac.kr>
2021-07-07 14:14:23 +0900

    exe workload has changed

--- exe_workload.sh
+++ exe_workload.sh
@@ -1,14 +1,26 @@
 #NOW=$(date)
 #echo NOW: ${NOW}
-EXE_TIME=$(date -d "+3 sec")
+#EXE_TIME=$(date -d "+3 sec")
 #echo ${EXE_TIME}
+
+NOW=$(date "+%s") #This will be standard input
+num=20
+plus=$(expr ${NOW} + ${num})
+echo ${plus}
+
+
+echo -n "Type timestamp to execute"
+read EXE_TIME
+echo ${EXE_TIME}
+
+
 while :
 do
-	NOW=$(date)
+	NOW=$(date "+%s")
 	#echo ${NOW}
 	#echo ${EXE_TIME}
 	if [[ ${NOW} == ${EXE_TIME} ]] ; then
-	  echo 3 sec later
+	  echo Execute
 	  break
 	fi
 done
