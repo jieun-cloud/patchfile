b95d9d0500f491c573d3d3ee30edba3bc65dcfe3
Jieun <pipiato@kaist.ac.kr>
2021-07-07 15:31:19 +0900

    lib script has changed

--- lib.sh
+++ lib.sh
@@ -1,10 +1,36 @@
 sudo apt-get install -y vim git ctags cscope libaio-dev libncurses5-dev libncursesw5-dev
-#git clone https://github.com/ESOS-Lab/barrieriostack.git
+
+#Format and mount nvme device
+sudo mkfs -t ext4 /dev/nvme0n1 
+sudo mount -t ext4 /dev/nvme0n1 /mnt
+ 
+git clone https://github.com/ESOS-Lab/barrieriostack.git
 git clone https://github.com/ESOS-Lab/Mobibench.git
-#mv ./barrieriostack /home/jieun
+mv ./barrieriostack /home/jieun
+
+#Mobibench
 mv ./Mobibench /home/jieun
 cd /home/jieun/Mobibench/shell
 sudo make -j50
 sudo make install
-sudo mkfs -t ext4 /dev/nvme0n1 
-sudo mount -t ext4 /dev/nvme0n1 /mnt
+
+#Filebench
+cd /home/jieun
+sudo apt-get update
+sudo apt-get install -y build-essential
+git clone https://github.com/filebench/filebench.git
+sudo apt-get install -y automake
+sudo apt-get install -y libtool
+sudo apt-get install -y bison flex
+sudo apt-get install -y make
+
+
+cd ./filebench
+libtoolize
+aclocal
+autoheader
+automake --add-missing
+autoconf
+./configure
+sudo make -j50
+sudo make install -j50
