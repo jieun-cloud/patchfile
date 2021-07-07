0883ec330bdf98692b846859483281747bf6af89
Jieun <pipiato@kaist.ac.kr>
2021-07-07 15:46:07 +0900

    kernel.sh has changed

--- kernel.sh
+++ kernel.sh
@@ -1,10 +1,9 @@
-cd /home/jieun/sata/barrieriostack/kernel/x86_64/
-#git checkout mq_barrier
-#cp /home/jieun/patchfile/patch_nvme_barrier /home/jieun/barrieriostack
-#patch -p0 < patch_nvme_barrier
-#cd ./kernel/x86_64
+cd /home/jieun/barrieriostack
+git checkout mq_barrier
+cp /home/jieun/patchfile/barrierpatch* ./
+patch -p0 < barrierpatch*
+cd ./kernel/x86_64
 KERNEL_CONF=$(uname -r)
-#ls -l /boot/ | grep config-${KERNEL_CONF}
 cp /boot/config-${KERNEL_CONF} ./.config
 sudo make menuconfig
 sudo make -j50
